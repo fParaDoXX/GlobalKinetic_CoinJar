99fc86bebcc239c09fe90af80712b942fa5b598f
Daniel Veldman <dev5@flowcentric-solutions.com>
2021-05-30 10:17:09 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GlobalKinetic_CoinJar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
