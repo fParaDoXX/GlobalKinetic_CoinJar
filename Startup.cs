8cb80e7500346410692c2f5b889880b37cd7e5ad
Daniel Veldman <dev5@flowcentric-solutions.com>
2021-05-31 08:00:54 +0200

    Revert "Core Functionality Working"
    This reverts commit 6a4c6680d4498a3e6bf17d6ebeed142598759924.

--- Startup.cs
+++ Startup.cs
@@ -1,9 +1,6 @@
-using GlobalKinetic_CoinJar.Data;
-using GlobalKinetic_CoinJar.ExternalClasses;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,22 +23,7 @@ namespace GlobalKinetic_CoinJar
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
-
-            services.AddDbContext<SysContext>(options =>
-             options.UseSqlServer(
-                 Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddControllersWithViews().AddRazorRuntimeCompilation();
-
-            
-            services.AddSingleton<ICoinJar, CoinJar>();
-
-
-
-            //Register MVC
-            services.AddMvc();
-
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
